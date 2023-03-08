89f19fe32e0e751c8423e8378158b06969602954
Musa Dulmiev <musadulmiev@gmail.com>
2023-03-08 14:08:53 +0300

    final (button clear)

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ balls_sp = []
 def draw_line(pos):
     if pos[1] >= screen_height - 100 or pos == [10000000000, 10000000]:
         return
-    k, b = get_k_and_b(start, pos)
+    k, b = get_k_and_b((x0, y0), pos)
     final = (-b // k, 0)
     pygame.draw.line(screen, pygame.Color("white"), (x0, y0), final, width=1)
 
 
 def spawn_balls(pos):
-    balls_sp.append(Ball(start, pos, screen, screen_size))
+    balls_sp.append(Ball((x0, y0), pos, screen, screen_size))
 
 
 if __name__ == '__main__':
@@ -23,25 +23,35 @@ if __name__ == '__main__':
     screen.fill(pygame.Color('black'))
     running = True
     clock = pygame.time.Clock()
-    yes = False
     pos = [10000000000, 10000000]
     x0 = screen_width // 2
     y0 = screen_height - 100
-    start = [x0, y0]
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.MOUSEMOTION:
                 pos = event.pos
-            if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.type == pygame.MOUSEBUTTONDOWN and pos[1] < y0:
                 spawn_balls(pos)
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP or event.key == pygame.K_w:
+                    y0 -= 100
+                elif event.key == pygame.K_DOWN or event.key == pygame.K_s:
+                    y0 += 100
+                    if y0 >= screen_height:
+                        y0 = screen_height
+                elif event.key == pygame.K_c:
+                    balls_sp.clear()
+
         clock.tick(600)
         pygame.display.flip()
         screen.fill(pygame.Color('black'))
-        draw_line(pos)
+        pygame.draw.line(screen, pygame.Color('white'), (0, y0), (screen_width, y0), width=3)
+        if pos[1] < y0:
+            draw_line(pos)
         if balls_sp:
             for ball in balls_sp:
-                ball.move()
+                ball.move(y0)
                 ball.render()
     pygame.quit()
--- objects/ball.py
+++ objects/ball.py
@@ -6,6 +6,8 @@ class Ball:
     def __init__(self, start, pos, screen, screen_size):
         self.radius = 10
         self.speed = 1
+        self.count_of_hit_board = 0
+        self.can_change_y = False
         self.sin = get_sin(start, pos)
         self.cos = get_cos(start, pos)
         self.color = pygame.Color((255, 255, 255))
@@ -15,10 +17,15 @@ class Ball:
         self.screen_width, self.screen_height = screen_size
         self.render()
 
-    def move(self):
+    def move(self, y_of_board):
         if self.x + self.radius >= self.screen_width or self.x <= 0:
             self.plus_x = -self.plus_x
-        if self.y + self.radius >= self.screen_height or self.y <= 0:
+        if self.y + self.radius < y_of_board:
+            self.can_change_y = True
+        if self.y + self.radius >= y_of_board and self.can_change_y:
+            self.y = y_of_board - 2 * self.radius
+            self.plus_y = -self.plus_y
+        if self.y <= 0:
             self.plus_y = -self.plus_y
         self.x = self.x + self.plus_x
         self.y = self.y + self.plus_y
