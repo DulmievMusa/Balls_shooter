8047c617fa4510b7567eea08dbcc8c0e95b75c69
Musa Dulmiev <musadulmiev@gmail.com>
2023-03-10 16:00:34 +0300

    lines among balls all

--- main.py
+++ main.py
@@ -20,9 +20,10 @@ def draw_lines_among_balls():
     if len(balls_sp) >= 2:
         pos = balls_sp[0].get_coords()
         for ball in balls_sp[1:]:
-            pygame.draw.line(screen, pygame.Color('white'), pos, ball.get_coords())
-            pos = ball.get_coords()
-    pygame.draw.line(screen, pygame.Color('white'), pos, balls_sp[0].get_coords())
+            for ball2 in balls_sp:
+                pygame.draw.line(screen, pygame.Color('white'), ball2.get_coords(), ball.get_coords())
+                # pos = ball.get_coords()
+        #pygame.draw.line(screen, pygame.Color('white'), pos, balls_sp[0].get_coords())
 
 
 if __name__ == '__main__':
